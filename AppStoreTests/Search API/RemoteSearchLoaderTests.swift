3eb831c7cd9950aab7a24a20e9886506f8e4ab77
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-01-25 11:37:52 +0300

    test delivers error on client error

--- AppStoreTests/Search API/RemoteSearchLoaderTests.swift
+++ AppStoreTests/Search API/RemoteSearchLoaderTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL,completion:@escaping ((Error) -> Void))
 }
 
 class RemoteSearchLoader {
@@ -16,13 +16,19 @@ class RemoteSearchLoader {
     private let url: URL
     private let client: HTTPClient
     
+    enum Error: Swift.Error {
+        case connectivity
+    }
+    
     init(url: URL,client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    func load() {
-        client.get(from: url)
+    func load(completion:@escaping ((Error) -> Void) = { _ in }) {
+        client.get(from: url) { _ in
+            completion(.connectivity)
+        }
     }
 }
 
@@ -53,6 +59,23 @@ class RemoteSearchLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs,[url,url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT(url: anyURL)
+        
+        let exp = expectation(description: "wait for completion")
+        var receievedError:RemoteSearchLoader.Error?
+        sut.load {
+            receievedError = $0
+            exp.fulfill()
+        }
+        
+        client.complete(with: anyNSError)
+        
+        wait(for: [exp], timeout: 0.01)
+        
+        XCTAssertEqual(receievedError , RemoteSearchLoader.Error.connectivity)
+    }
+    
     // MARK:  Helpers
     
     private func makeSUT(
@@ -73,11 +96,24 @@ class RemoteSearchLoaderTests: XCTestCase {
         URL(string: "https://any-given-url.com")!
     }
     
+    private var anyNSError: NSError {
+        NSError(domain: "any error", code: 1)
+    }
+    
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
         
-        func get(from url: URL) {
-            requestedURLs.append(url)
+        private(set) var messages = [(url: URL,completion: (Error) -> Void)]()
+       
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        
+        func get(from url: URL,completion:@escaping ((Error) -> Void)) {
+            messages.append((url,completion))
+        }
+        
+        func complete(with error: Error,at index: Int = 0) {
+            messages[index].completion(error)
         }
     }
     
