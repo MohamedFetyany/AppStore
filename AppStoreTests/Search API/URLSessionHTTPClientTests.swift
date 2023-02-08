13f0aac4d2595375065b7eef300a477b934d5d52
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-02-08 09:14:09 +0300

    Resumes data task on creation

--- AppStoreTests/Search API/URLSessionHTTPClientTests.swift
+++ AppStoreTests/Search API/URLSessionHTTPClientTests.swift
@@ -16,7 +16,7 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in}
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
@@ -32,18 +32,47 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs,[url])
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url,with: task)
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount,1)
+    }
+    
     // MARK:  Helpers
     
     private class URLSessionSpy: URLSession {
         
         var receivedURLs = [URL]()
         
+        var stub = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL,with task: URLSessionDataTask) {
+            stub[url] = task
+        }
+        
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
             
-            return FakeURLSessionDataTask()
+            return stub[url] ?? FakeURLSessionDataTask()
         }
+    
     }
     
-    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {}
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
 }
