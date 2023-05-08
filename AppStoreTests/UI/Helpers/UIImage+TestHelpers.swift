c27c1dd7c5fe0f83fdac895b64441c6086c55af3
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-05-08 09:49:57 +0300

    Extract UIImage test helper to a separate file

---
+++ AppStoreTests/UI/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+TestHelpers.swift
+//  AppStoreTests
+//
+//  Created by Mohamed Ibrahim on 08/05/2023.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
+    }
+}
